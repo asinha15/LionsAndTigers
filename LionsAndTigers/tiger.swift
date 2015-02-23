f458a39db99867eda31815a8b5de75c96c836c46
Arnab Sinha <arisin@gmail.com>
2015-02-22 16:14:52 -0800

    first commit

---
+++ LionsAndTigers/tiger.swift
@@ -0,0 +1,9 @@
+//
+//  tiger.swift
+//  LionsAndTigers
+//
+//  Created by Arnab Sinha on 2/20/15.
+//  Copyright (c) 2015 Arnab Sinha. All rights reserved.
+//
+
+import Foundation
---
+++ Tiger.swift
@@ -0,0 +1,18 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Arnab Sinha on 2/20/15.
+//  Copyright (c) 2015 Arnab Sinha. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var breed = ""
+    var name = ""
+    var image = UIImage(named:"")
+    
+}
